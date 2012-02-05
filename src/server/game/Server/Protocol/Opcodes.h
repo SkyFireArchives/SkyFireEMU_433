7dd95819e6476a38f7504146195dbc6f736a1345
SkyFire <admin@projectskyfire.org>
2012-02-05 11:43:47 -0600

    Tab cleanup.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1370,8 +1370,8 @@ enum Opcodes
     CMSG_OBJECT_UPDATE_FAILURE                       = 0x00, /* old 0x07233 */
     SMSG_VERIFY_CONNECTIVITY                         = 0x00, /* old 0x04F57 */
     CMSG_VERIFY_CONNECTIVITY_RESPONSE                = 0x10002, /* old 0x04C52 */
-	SMSG_USERLIST_REMOVE                             = 0x00,
-	SMSG_WAIT_QUEUE_UPDATE                           = 0x00CE1,
+    SMSG_USERLIST_REMOVE                             = 0x00,
+    SMSG_WAIT_QUEUE_UPDATE                           = 0x00CE1,
 };
 
 extern void InitOpcodeTable();
