6ae2ef19457d807762932c42a86da1c23f186f2d
Sovak <Hacker007@zoznam.sk>
2012-02-05 18:39:11 +0100

    SMSG_CREATURE_QUERY_RESPONSE -- needs mysql edit
    Signed-off-by: Sovak <Hacker007@zoznam.sk>

--- src/server/game/Handlers/QueryHandler.cpp
+++ src/server/game/Handlers/QueryHandler.cpp
@@ -129,9 +129,11 @@ void WorldSession::HandleCreatureQueryOpcode(WorldPacket & recv_data)
         data << uint32(entry);                              // creature entry
         data << Name;
         data << uint8(0) << uint8(0) << uint8(0);           // name2, name3, name4, always empty
+        data << uint8(0) << uint8(0) << uint8(0) << uint8(0); // Unk string 4x      
         data << SubName;
         data << ci->IconName;                               // "Directions" for guard, string for Icons 2.3.0
         data << uint32(ci->type_flags);                     // flags
+        data << uint32(0);                                  // flags 2 -- 70% possiblle coz of lopping with flags 1
         data << uint32(ci->type);                           // CreatureType.dbc
         data << uint32(ci->family);                         // CreatureFamily.dbc
         data << uint32(ci->rank);                           // Creature Rank (elite, boss, etc)
