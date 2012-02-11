ba9c69135a2c0a55fb4093e589a847147ba6cb17
Sovak <Sovak007@gmail.com>
2012-02-11 14:49:33 +0100

    It should be ok now
    Signed-off-by: Sovak <Sovak007@gmail.com>

--- src/server/game/Handlers/AuthHandler.cpp
+++ src/server/game/Handlers/AuthHandler.cpp
@@ -26,12 +26,11 @@ void WorldSession::SendAuthResponse(uint8 code, bool shortForm, uint32 queuePos)
     WorldPacket packet(SMSG_AUTH_RESPONSE, 1 + 4 + 1 + 4 + 1 + (shortForm ? 0 : (4 + 1)));
     packet << uint8(code);
 
-	packet.WriteBit(!shortForm); // Queue Info
-	packet.WriteBit(1); // hasAccoutInfo
+	packet << uint8(1 << 7);
 
     if (!shortForm)    
     {
-        packet << uint8(0);                                // Unk 3.3.0
+		packet << uint8(0);                                // Unk 3.3.0
         packet << uint32(queuePos);                        // Queue position
     }
 
